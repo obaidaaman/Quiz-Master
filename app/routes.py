7ff280556a5edf79acb8db8da7de51209877c871
obaidaaman <amanobaidofficial01@gmail.com>
2025-03-14 16:39:23 +0530

    database has been created and tested

--- app/routes.py
+++ app/routes.py
@@ -1,13 +1,13 @@
 from app import create_app, db
 from flask import render_template
+from app.models import User, Subject  # Import models
 
 app = create_app()
 
 @app.cli.command('db-create')
 def create_db():
-    db.create_all()
-    print("Database created!")
-
+        db.create_all()
+        print("Database created!")
 
 @app.route("/")
 def home():
